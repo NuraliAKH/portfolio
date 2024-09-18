2621a5775cb8a8f9160e2e467c21fa1906723a50
NuraliAKH <axmadovnurali5@gmail.com>
2024-09-18 10:11:36 +0500

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,240 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  soff,
+  tavakkalna,
+  upwork,
+  tripguide,
+  speed,
+  kino,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "Frontend Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Web Desiner",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "React.js Developer",
+    company_name: "Soff Hub",
+    icon: soff,
+    iconBg: "#383E56",
+    date: "June 2023 - August 2023",
+    points: [
+      "Development of responsive websites",
+      "API integration",
+      "Creation of online stores",
+      "Using various libraries",
+      "Working with Redux toolkit / Rematch",
+      "Optimization of components",
+      "Converting designs from Figma to React",
+      "Participating in code reviews and providing constructive feedback to other developers",
+    ],
+  },
+  {
+    title: "Freelance",
+    company_name: "Upwork",
+    icon: upwork,
+    iconBg: "#E6DEDD",
+    date: "August 2023 - Зкуыуте",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+    company_name: "Tavakkalna",
+    icon: tavakkalna,
+    iconBg: "#E6DEDD",
+    date: "Murch 2023 - Present",
+    points: [
+      "Building CRUD APIs using NestJS and Prisma.js",
+      "Designing and managing PostgreSQL databases",
+      "Developing CRUD pages with React and TypeScript",
+      "Collaborating with the team via GitHub and Linear",
+      "Working with clients to gather feedback and clarify requirements",
+      "Discussing, testing, and fixing bugs",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial: "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial: "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial: "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Type Speed",
+    description:
+      "Web application for practicing typing speed and accuracy with customizable tests, detailed statistics, and progress tracking.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: speed,
+    source_code_link: "https://github.com/NuraliAKH/Typing-Speed-Trainer",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application for browsing and streaming movies, offering detailed information, user reviews, and personalized recommendations.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: kino,
+    source_code_link: "https://github.com/NuraliAKH",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/NuraliAKH",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
